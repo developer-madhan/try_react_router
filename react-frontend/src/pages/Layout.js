ca5af2b2b8b902f76c48f2b20b2c5f2290191700
atpcs011 <atpcs011@outlook.com>
2024-03-22 11:59:19 +0530

    added bootstrap style

--- react-frontend/src/pages/Layout.js
+++ react-frontend/src/pages/Layout.js
@@ -1,26 +1,35 @@
 // Layout.js page
 import { Outlet, Link } from "react-router-dom";
+import { Container, Col, Row } from "react-bootstrap";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <Container>
+        <Row className="align-items-baseline my-3" >
+          <Col md={2}>
+            <img src="https://picsum.photos/150/50" alt="" />
+          </Col>
+          <Col md={8}>
+            <nav>
+              <ul className="list-unstyled d-flex justify-content-around align-content-end">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/blogs">Blogs</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </Col>
+        </Row>
+      </Container>
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
